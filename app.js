d7b29456ca2276395e17faa1e1c88f93853185fb
Kevin Yogi <kevinyogi@Kevins-MacBook-Pro.local>
2017-10-06 19:47:23 -1000

    fixed put method

--- app.js
+++ app.js
@@ -20,26 +20,26 @@ app.get('/buzzwords', (req, res) => {
   res.json({"buzzWords": theBuzzWords});
 });
 
-app.post('/buzzword', urlencodedParser, (req, res) => {
+app.route('/buzzword')
+.post(urlencodedParser, (req, res) => {
   if(!req.body){
     return res.sendStatus(400);
   }else if(theBuzzWords.length === 0){
     theBuzzWords.push(req.body);
     res.json({"success": true});
   }else{
-    for(let i = 0; i < theBuzzWords.length; i++){
-      if(req.body.buzzWord === theBuzzWords[i].buzzWord){
-        res.sendStatus(400);
-      }else{
-        console.log(req.body.buzzWord, theBuzzWords[i].buzzWord);
-        theBuzzWords.push(req.body);
-        res.json({"success": true});
-      }
+    let wordIsThere = theBuzzWords.some((element) => {
+      return element.buzzWord === req.body.buzzWord;
+    })
+    if(wordIsThere === true){
+      return res.sendStatus(400);
+    }else{
+      theBuzzWords.push(req.body);
+      res.json({"succes": true});
     }
   }
-});
-
-app.put('/buzzword', urlencodedParser, (req, res) => {
+})
+.put(urlencodedParser, (req, res) => {
   if(!req.body){
     return res.sendStatus(400);
   }else{
@@ -48,6 +48,22 @@ app.put('/buzzword', urlencodedParser, (req, res) => {
         theBuzzWords[i].heard = 'true';
         score += Number(theBuzzWords[i].points);
         res.json( {"success": true, "newScore": score} );
+      }else{
+        res.sendStatus(400);
+      }
+    }
+  }
+})
+.delete(urlencodedParser, (req, res) => {
+  if(!req.body){
+    res.sendStatus(400);
+  }else{
+    for(let i = 0; i < theBuzzWords.length; i++){
+      if(req.body.buzzWord !== theBuzzWords[i].buzzWord){
+        res.sendStatus(404);
+      }else if(req.body.buzzWord === theBuzzWords[i].buzzWord){
+        theBuzzWords.splice(i, 1);
+        res.json( {"success": true} );
       }
     }
   }
